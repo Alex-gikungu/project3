a4f83f4985200c6ebbc38212b06f2c23d4bcd9fb
Alex-gikungu <alexigikungu.012@gmail.com>
2023-05-22 14:44:42 +0300

    updated readme file

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
-const cards = document.querySelectorAll('.card');
+/*
+const startButton = document.getElementById("startbtn");
+const movesElement = document.getElementById("moves");
+const timerElement = document.getElementById("timer");
+
+let movesCount = 0;
+let seconds = 0;
+let minutes = 0;
+let timer;
+let clickedCards = 0;
+
+ //Event listener for the start button
+startButton.addEventListener("click", startGame);
+
+// Function to start the game
+function startGame() {
+    // Reset moves count, timer, and clicked cards count
+    movesCount = 0;
+    seconds = 0;
+    minutes = 0;
+    clickedCards = 0;
+    movesElement.textContent = "0 move(s)";
+    timerElement.textContent = "Time: 0 mins 0 sec";
+
+    // Remove event listeners from cards
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener("click", handleCardClick);
+    }
+
+    // Start the timer
+    timer = setInterval(updateTimer, 1000);
+}
+
+// Function to update the timer
+function updateTimer() {
+    seconds++;
+    if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+    }
+    timerElement.textContent = `Time: ${minutes} mins ${seconds} sec`;
+}
+
+ const cards = document.querySelectorAll('.card');
 let cardOne = null;
 let cardTwo = null;
-/*
+
+cards.forEach(card => {
+    card.addEventListener('click', flipCard);});
 function flipCard(e) {
   let clickedCard = e.target.parentNode;
 
@@ -22,7 +67,7 @@ function flipCard(e) {
 }
 
 function matchCards(img1, img2) {
-  if (img1 === img2) { // if two card images matched
+  if (img1 === img2) { // if two card images matches
     cardOne.removeEventListener('click', flipCard);
     cardTwo.removeEventListener('click', flipCard);
     cardOne = cardTwo = '';
@@ -30,7 +75,7 @@ function matchCards(img1, img2) {
     setTimeout(() => {
       cardOne.classList.add('vibration');
       cardTwo.classList.add('vibration');
-    }, 400);
+    }, 100);
 
     setTimeout(() => {
       cardOne.classList.remove('vibration', 'flip');
@@ -40,7 +85,39 @@ function matchCards(img1, img2) {
   }
 }
 
-cards.forEach(card => {
-  card.addEventListener('click', flipCard);
-});
-*/
+  // Get the necessary elements
+
+// Function to handle card clicks
+function handleCardClick() {
+    // Increment moves count
+    movesCount++;
+    movesElement.textContent = `${movesCount} move(s)`;
+
+    // Check if all cards have been clicked
+    clickedCards++;
+    if (clickedCards === cards.length) {
+        // Stop the timer
+        clearInterval(timer);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
